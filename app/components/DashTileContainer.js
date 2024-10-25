66c7560579231b865b622c2286e060da3517c3e7
Frederick Zhang <frederickzhang@ucla.edu>
2024-10-24 21:45:18 -0700

    BIG: redo dashboard logic for new format

--- app/components/DashTileContainer.js
+++ app/components/DashTileContainer.js
@@ -19,25 +19,21 @@ const DashTileContainer = ({ matches, matchType, onTileClick }) => {
               >
                 <DashboardTile
                   matchName={`${match.opponent} ${match.date}`}
-                  clientTeam={match.clientTeam}
-                  opponentTeam={match.opponentTeam}
-                  player1Name={match.clientPlayer}
-                  player2Name={match.opponentPlayer}
-                  player1FinalScores={Object.values(match.matchScore).map(
-                    (set) => ({
-                      score: set ? set.clientGames : null
-                    })
+                  clientTeam={match.teams.clientTeam}
+                  opponentTeam={match.teams.opponentTeam}
+                  player1Name={`${match.players.client.firstName} ${match.players.client.lastName}`}
+                  player2Name={`${match.players.opponent.firstName} ${match.players.opponent.lastName}`}
+                  player1FinalScores={match.sets.map((set) => ({
+                    score: set ? set.clientGames : null
+                  }))}
+                  player2FinalScores={match.sets.map((set) => ({
+                    score: set ? set.opponentGames : null
+                  }))}
+                  player1TieScores={match.sets.map((set) =>
+                    set ? set.clientTiebreak : null
                   )}
-                  player2FinalScores={Object.values(match.matchScore).map(
-                    (set) => ({
-                      score: set ? set.opponentGames : null
-                    })
-                  )}
-                  player1TieScores={Object.values(match.matchScore).map(
-                    (set) => (set ? set.clientTiebreak : null)
-                  )}
-                  player2TieScores={Object.values(match.matchScore).map(
-                    (set) => (set ? set.opponentTiebreak : null)
+                  player2TieScores={match.sets.map((set) =>
+                    set ? set.opponentTiebreak : null
                   )}
                   isUnfinished={false}
                   isTagged={match.isTagged}
