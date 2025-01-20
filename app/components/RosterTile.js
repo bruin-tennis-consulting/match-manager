7b42e1821f4e3a0939af29e87c835f26490ddf47
Alexander Chen <alexander.chen750@gmail.com>
2025-01-20 11:52:58 -0800

    add links for roster list

--- app/components/RosterTile.js
+++ app/components/RosterTile.js
@@ -1,27 +1,21 @@
-import React, { useState } from 'react'
-
+import Link from 'next/link'
 import styles from '@/app/styles/Roster.module.css'
-const RosterTile = ({ firstName, lastName, playerPhoto }) => {
-  const [playerClicked, setPlayerClicked] = useState(false)
 
-  const handleClick = () => {
-    setPlayerClicked(!playerClicked)
-  }
+const RosterTile = ({ firstName, lastName, playerPhoto }) => {
+  const playerId = `${firstName.toLowerCase()}-${lastName.toLowerCase()}`
 
   return (
     <div className={styles.playerContainer}>
-      <div
-        className={styles.infoContainer}
-        style={{ backgroundColor: playerClicked ? '#F1F9FF' : '#FFFFFF' }}
-        onClick={handleClick} // Handle click event
-      >
-        <img className={styles.playerImage} src={playerPhoto} alt="" />
-        <div className={styles.textContainer}>
-          <div className={styles.playerName}>
-            {firstName} {lastName}
+      <Link href={`/profile/${playerId}`} style={{ textDecoration: 'none' }}>
+        <div className={styles.infoContainer}>
+          <img className={styles.playerImage} src={playerPhoto} alt="" />
+          <div className={styles.textContainer}>
+            <div className={styles.playerName}>
+              {firstName} {lastName}
+            </div>
           </div>
         </div>
-      </div>
+      </Link>
     </div>
   )
 }
