f7d5df1d24a27c0c443d57a2a394d82c01d8e797
bzhang102 <yuxiangbrandonzhang@gmail.com>
2025-02-08 15:44:54 -0800

    updated img tag to next's Image component

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   experimental: {
     missingSuspenseWithCSRBailout: false
+  },
+  images: {
+    domains: ['firebasestorage.googleapis.com']
   }
 }
+
+module.exports = nextConfig
